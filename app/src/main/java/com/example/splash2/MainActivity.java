0aaac143ca14b8e0745585b53d8312dcb86da7ad
jkpoter <yorel101.9@gmail.com>
2020-03-22 23:02:47 -0400

    Initial commit

---
+++ app/src/main/java/com/example/splash2/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.splash2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        setTheme(R.style.AppTheme);
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
